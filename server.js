9592f13fc4584d9d2c9ca7397dddbc483e24ca43
Aicha Fall <faallaicha97@gmail.com>
2024-10-09 16:54:34 +0000

    second commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Pour servir les fichiers statiques à partir du dossier dist
+app.use(express.static(__dirname + '/dist/'));
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/portfolio-omar/index.html'));
+});
+
+// Démarrer le serveur sur le port défini par Heroku ou sur le port 8080
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
