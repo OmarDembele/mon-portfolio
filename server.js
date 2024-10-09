45fd5f3ada1b0ba6978811df5f680b9f55f0fbbc
Aicha Fall <faallaicha97@gmail.com>
2024-10-09 17:00:16 +0000

    second commit

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 
 // Pour servir les fichiers statiques à partir du dossier dist
-app.use(express.static(__dirname + '/dist/'));
+app.use(express.static(__dirname + '/dist/portfolio-omar'));
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/portfolio-omar/index.html'));
+  res.sendFile('index.html', {root: '/dist/portfolio-omar/'});
 });
 
 // Démarrer le serveur sur le port défini par Heroku ou sur le port 8080
